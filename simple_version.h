5c2194a5be7497dcb7536cfeda89d9df3e3b2742
Vostrikova Kira <kirakaramisheva2007@gmail.com>
2026-04-18 19:13:39 +0300

    Delete simple_version.h

--- simple_version.h
+++
@@ -1,5 +0,0 @@
-const int nmax = 256;
-const float r2max = 5.0f;
-
-void DrawMandelbrot(HDC *dc, COLORREF buf[600][800], double *x_start, double *y_start, double *zoom);
-void ReallyDraw(HDC *dc, COLORREF buf[600][800]);
